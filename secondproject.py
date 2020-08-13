c4679e53d00e5d54b571a716e421ce3411989710
unknown <ngbejume5@gmail.com>
2020-08-13 09:15:04 +0100

    added new code

---
+++ secondproject.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 08:36:20 2020
+
+@author: Mani
+"""
+
+print("this is the second assignment of Team 5")
+print("code workaholics")
+print("an if condition with the continue keyword")
+
+print("for example")
+var = 30
+for number in range(var):       
+    if number == 5:
+        continue
+    print("current value is now:", end=" ")
+    print(number + 1) 
+    if number == 26:
+        break    
+print("done")    
+print("good bye")
